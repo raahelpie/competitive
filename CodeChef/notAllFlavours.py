9ee77af9cd4635d4e603f0184dfc1289a1e76f96
Raahel Baig <17131a05h5@gvpce.ac.in>
2020-06-25 12:34:04 +0530

    NOTALLFL correct solution added

--- CodeChef/notAllFlavours.py
+++ CodeChef/notAllFlavours.py
@@ -32,14 +32,55 @@ t = int(input())
 def longest_sub():
     n, k = map(int, input().split())
     flavours = list(map(int, input().split()))
-    eachLarge = []
-    for i in range(n-1):
-        for j in range(n-1, i, -1):
-            c = len(set(flavours[i:j+1]))
-            if c < k:
-                eachLarge.append(len(flavours[i:j+1]))
-                break
-    return max(eachLarge)
-
+    # seen = set()
+    # start = {}
+    # end = {}
+    # unique = 0
+    # maxi = {}
+    # for i in range(n):
+    #     if flavours[i] not in seen:
+    #         start[flavours[i]] = i
+    #         end[flavours[i]] = i
+    #         seen.add(flavours[i])
+    #         unique += 1
+    #         maxi[flavours[i]] = -1
+    #     else:
+    #         maxi[flavours[i]] = max(maxi[flavours[i]], i - end[flavours[i]] - 1)
+    #         end[flavours[i]] = i
+    # print(start)
+    # print(end)
+    # if unique < k:
+    #     return n
+    # else:
+    #     gans = 0
+    #     for each in seen:
+    #         ans = max(start[each], n - 1 - end[each], maxi[each])
+    #         gans = max(ans, gans)
+    #     return gans
+    st = set()
+    count = {}
+    start, res = 0, 0
+    for i in range(n):
+        if flavours[i] in st:
+            count[flavours[i]] += 1
+        else:
+            st.add(flavours[i])
+            count[flavours[i]] = 1
+        if len(st) == k:
+            while len(st) == k:
+                count[flavours[start]] -= 1
+                if count[flavours[start]] == 0:
+                    st.remove(flavours[start])
+                start += 1
+        res = max(res, i - start + 1)
+    return res
 for i in range(t):
     print(longest_sub())
+
+"""
+10 4
+1 2 1 2 1 2 4 3 3 1
+
+set = {2: 1, 4: 1, 3: 1}
+
+"""
